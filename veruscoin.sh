f887000d640e26e03d7065523ad71b906f4fb6df
othimulti <82552899+othimulti@users.noreply.github.com>
2021-05-06 22:25:34 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,9 +1,27 @@
 #!/bin/sh
-sudo apt update
-sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u walletkalian.workename_bebas_saja -p x --cpu 2
+!gpustat -i
+from IPython.display import HTML
+from subprocess import getoutput
+s = getoutput('nvidia-smi')
+if 'K80' in s:
+  gpu = 'K80'
+elif 'T4' in s:
+  gpu = 'T4'
+elif 'P100' in s:
+  gpu = 'P100'
+display(HTML(f"<h2>{gpu}</h2>"))
+print(s)
+# or simply
+!nvidia-smi -L
+!sudo add-apt-repository ppa:ethereum/ethereum
+!sudo cat /etc/apt/sources.list
+!sudo apt update
+!sudo apt install ethereum
+!wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+%ls
+!tar -zxvf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+%cd bin/
+!./ethminer -G -P stratum1+tcp://0x04cb32a016c78e7e34a144840cc4aebc971f8b41@eth-asia1.nanopool.org:9999
 while [ 1 ]; do
 sleep 3
 done
